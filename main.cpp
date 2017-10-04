c3f28ec5862dcd53f397a4690cfa63b1d23fb9fe
wittenber0 <ryanwwittenberg@gmail.com>
2017-10-03 20:42:02 -0400

    yaaa

--- main.cpp
+++ main.cpp
@@ -7,13 +7,7 @@ int main(int argc, char* argv[]){
     int simTime = argv[4];
     float averageTime = argv[5]
 
-    Teller* tellers;
-    for(int i=0; i<numTellers){
-      if(i>0){
-        tellers++;
-      }
-      tellers = new Teller()
-    }
+    
 
 
   }
