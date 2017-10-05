2969a22382f9bb81eb754e5b173d8f35470e28a3
wittenber0 <ryanwwittenberg@gmail.com>
2017-10-05 17:41:34 -0400

    compile

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@
 #include "tellerEvent.h"
 #include "tellerQue.h"
 
+double globalclock;
+double totalIdleTime=0;
+double totalServiceTime=0;
+int totalCustomersServed=0;
+double totalCustomerMinutes=0;
+double maximumWaitTime=0;
+double finishTime =0;
+int emptybank = 0;
+double standardDeviation = 0;
 
 
 
